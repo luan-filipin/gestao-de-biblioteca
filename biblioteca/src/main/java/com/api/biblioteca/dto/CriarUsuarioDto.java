4f5674f0ed58be47e98ba2434ef7122d86deb5cf
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-06 19:31:15 -0300

    Dto
    - Criado Dto para a criação do usuário.
    - Adicionado as anotações para validação dos campos.

---
+++ biblioteca/src/main/java/com/api/biblioteca/dto/CriarUsuarioDto.java
@@ -0,0 +1,17 @@
+package com.api.biblioteca.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record CriarUsuarioDto(
+		@NotBlank(message = "O nome é obrigatorio!") String nome,
+		@Email(message = "Email invalido!") String email,
+		@NotBlank(message = "O telefone é obrigatorio!") 
+		@Pattern(
+			    regexp = "\\(?\\d{2}\\)?\\s?9?\\d{4}-?\\d{4}",
+			    message = "Telefone inválido. Formato esperado: (11) 91234-5678"
+			)
+		String telefone) {
+
+}
