74e536d7b6f0172f7b7d60db8b301de21dc18fc0
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-07 10:53:46 -0300

    Dto
    - Implementado a anotação @NotBlank no email e organizado a estrutura para melhor compreensão

--- biblioteca/src/main/java/com/api/biblioteca/dto/CriarUsuarioDto.java
+++ biblioteca/src/main/java/com/api/biblioteca/dto/CriarUsuarioDto.java
@@ -5,13 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record CriarUsuarioDto(
-		@NotBlank(message = "O nome é obrigatorio!") String nome,
-		@Email(message = "Email invalido!") String email,
+		@NotBlank(message = "O nome é obrigatorio!") 
+		String nome,
+		
+		@NotBlank(message = "O email é obrigatorio!")
+		@Email(message = "Email invalido!") 
+		String email,
+		
 		@NotBlank(message = "O telefone é obrigatorio!") 
-		@Pattern(
-			    regexp = "\\(?\\d{2}\\)?\\s?9?\\d{4}-?\\d{4}",
-			    message = "Telefone inválido. Formato esperado: (11) 91234-5678"
-			)
+		@Pattern(regexp = "\\(?\\d{2}\\)?\\s?9?\\d{4}-?\\d{4}",message = "Telefone inválido. Formato esperado: (11)91234-5678")
 		String telefone) {
 
 }
