40529c54dac478882c6637f7fd0d026ed2e9085d
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-07 15:57:20 -0300

    GlobalExceptionLivrosHandler
    - Implementado tratamento global exception para melhor compreensão dos erros.

---
+++ biblioteca/src/main/java/com/api/biblioteca/config/GlobalExceptionLivrosHandler.java
@@ -0,0 +1,98 @@
+package com.api.biblioteca.config;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.api.biblioteca.dto.response.ErroCampoDto;
+import com.api.biblioteca.dto.response.ErroRespostaDto;
+import com.api.biblioteca.exception.IsbnDiferenteDoCorpoException;
+import com.api.biblioteca.exception.IsbnInexistenteException;
+import com.api.biblioteca.exception.IsbnJaExisteException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionLivrosHandler {
+	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErroRespostaDto> handlerGenerico(RuntimeException ex, HttpServletRequest request){
+		ErroRespostaDto erro = new ErroRespostaDto(
+				ex.getMessage(),
+				HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				request.getRequestURI());
+	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
+	}
+
+	@ExceptionHandler(IsbnDiferenteDoCorpoException.class)
+	public ResponseEntity<ErroRespostaDto> handlerIsbnDiferenteDoCorpo(IsbnDiferenteDoCorpoException ex, HttpServletRequest request){
+		ErroRespostaDto erro = new ErroRespostaDto(
+				ex.getMessage(), 
+				HttpStatus.BAD_REQUEST.value(),
+				request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+	
+	@ExceptionHandler(IsbnInexistenteException.class)
+	public ResponseEntity<ErroRespostaDto> handlerIsbnInexistente(IsbnInexistenteException ex, HttpServletRequest request){
+		ErroRespostaDto erro = new ErroRespostaDto(
+				ex.getMessage(), 
+				HttpStatus.NOT_FOUND.value(),
+				request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+	
+	@ExceptionHandler(IsbnJaExisteException.class)
+	public ResponseEntity<ErroRespostaDto> handlerIsbnJaExiste(IsbnJaExisteException ex, HttpServletRequest request){
+		ErroRespostaDto erro = new ErroRespostaDto(
+				ex.getMessage(), 
+				HttpStatus.CONFLICT.value(), 
+				request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+	}
+	
+	//Esse handler é disparado quando a falha de validação em objetos do corpo da requisição.
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErroRespostaDto> handlerMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest request){
+		
+		List<ErroCampoDto> erros = ex.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.map(erro -> new ErroCampoDto(erro.getField(), erro.getDefaultMessage()))
+				.toList();
+		
+		ErroRespostaDto resposta = new ErroRespostaDto(
+				"Campos inválidos",
+				HttpStatus.BAD_REQUEST.value(), 
+				request.getRequestURI(),
+				erros);
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resposta);
+	}
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ErroRespostaDto> handerconstraintViolation(ConstraintViolationException ex, HttpServletRequest request){
+		
+		List<ErroCampoDto> erros = ex.getConstraintViolations()
+				.stream()
+				.map(violation -> new ErroCampoDto(
+						violation.getPropertyPath().toString(), 
+						violation.getMessage()))
+				.toList();
+		
+		ErroRespostaDto resposta = new ErroRespostaDto(
+				"Campos inválidos",
+				HttpStatus.BAD_REQUEST.value(),
+				request.getRequestURI(),
+				erros);
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resposta);
+	}
+	
+}
