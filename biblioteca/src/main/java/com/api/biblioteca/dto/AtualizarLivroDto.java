c0a0211bf500adacfb069d5c76d099775fa4a2e0
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-07 19:53:17 -0300

    Dto
    - Implementado a validação para os campos.

--- biblioteca/src/main/java/com/api/biblioteca/dto/AtualizarLivroDto.java
+++ biblioteca/src/main/java/com/api/biblioteca/dto/AtualizarLivroDto.java
@@ -2,11 +2,19 @@ package com.api.biblioteca.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record AtualizarLivroDto(
+		@NotBlank(message = "O campo titulo é obrigatorio!")
 		String titulo,
+		@NotBlank(message = "O campo autor é obrigatorio!")
 		String autor,
+		@NotBlank(message = "O campo isbn é obrigatorio!")
 		String isbn,
+		@NotBlank(message = "O campo categoria é obrigatorio!")
 		String categoria,
+		@NotNull(message = "O campo dataPublicacao é obrigatorio!")
 		LocalDate dataPublicacao) {
 
 }
