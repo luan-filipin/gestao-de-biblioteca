25b93cb65c5d69a1d515e17af4af3ec6f0d4444b
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-07 10:52:36 -0300

    Dto
    - Adicionado no Dto as validações do campo para que seja obrigatórias e não sejam nulas.

--- biblioteca/src/main/java/com/api/biblioteca/dto/response/AtualizaUsuarioDto.java
+++ biblioteca/src/main/java/com/api/biblioteca/dto/response/AtualizaUsuarioDto.java
@@ -1,4 +1,18 @@
 package com.api.biblioteca.dto.response;
 
-public record AtualizaUsuarioDto(String nome, String email, String telefone) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record AtualizaUsuarioDto(
+		@NotBlank(message = "O nome é obrigatorio!") 
+		String nome, 
+		
+		@NotBlank(message = "O email é obrigatorio!")
+		@Email(message = "Email invalido!")
+		String email, 
+		
+		@NotBlank(message = "O telefone é obrigatorio!") 
+		@Pattern(regexp = "\\(?\\d{2}\\)?\\s?9?\\d{4}-?\\d{4}", message = "Telefone inválido. Formato esperado: (11)91234-5678")
+		String telefone) {
 }
