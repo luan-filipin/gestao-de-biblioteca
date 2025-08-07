42d7efcea9f3b4b7f95df165a297f6b3dcd92fb8
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-07 19:54:03 -0300

    Teste
    - Implementado os teste de sucesso e falha para o controller de deletar.
    - Implementado os teste de sucesso e falha para o controller de Atualizar.

--- biblioteca/src/test/java/com/api/biblioteca/controller/LivroControllerTest.java
+++ biblioteca/src/test/java/com/api/biblioteca/controller/LivroControllerTest.java
@@ -1,5 +1,6 @@
 package com.api.biblioteca.controller;
 
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.api.biblioteca.config.GlobalExceptionLivrosHandler;
+import com.api.biblioteca.dto.AtualizarLivroDto;
 import com.api.biblioteca.dto.CriarLivroDto;
 import com.api.biblioteca.dto.response.LivroDto;
 import com.api.biblioteca.service.LivroService;
@@ -30,33 +32,120 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc(addFilters = false)
 @Import(GlobalExceptionLivrosHandler.class)
 class LivroControllerTest {
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
 
 	@MockitoBean
 	private LivroService livroService;
-	
+
 	@DisplayName("POST - Deve criar um livro quando a requisição for valida.")
 	@Test
-	void deveCriarLivroSeRequisicaoForValida() throws Exception{
+	void deveCriarLivroSeRequisicaoForValida() throws Exception {
+
+		LocalDate dataFixa = LocalDate.of(2025, 8, 7);
+		CriarLivroDto dtoEntrada = new CriarLivroDto("Clean Code", "Robert C. Martin", "9780132350884", "Programação");
+		LivroDto dtoEsperado = new LivroDto(1L, "Clean Code", "Robert C. Martin", "9780132350884", dataFixa,
+				"Programação");
+
+		when(livroService.criarLivro(dtoEntrada)).thenReturn(dtoEsperado);
+
+		mockMvc.perform(post("/api/livros").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dtoEntrada))).andExpect(status().isCreated())
+				.andExpect(jsonPath("$.id").value(1)).andExpect(jsonPath("$.titulo").value("Clean Code"))
+				.andExpect(jsonPath("$.autor").value("Robert C. Martin"))
+				.andExpect(jsonPath("$.isbn").value("9780132350884"))
+				.andExpect(jsonPath("$.categoria").value("Programação"));
+	}
+
+	@DisplayName("POST - Deve lançar exception se houver campo invalido.")
+	@Test
+	void deveLancarExceptionSeHouverCampoInvalido() throws Exception {
+
+		CriarLivroDto dtoEntrada = new CriarLivroDto("", "Robert C. Martin", "9780132350884", "Programação");
+
+		mockMvc.perform(post("/api/livros").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dtoEntrada))).andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.mensagem").value("Campos inválidos")).andExpect(jsonPath("$.status").value(400))
+				.andExpect(jsonPath("$.path").value("/api/livros")).andExpect(jsonPath("$.timestamp").exists())
+				.andExpect(jsonPath("$.erros[0].campo").value("titulo"))
+				.andExpect(jsonPath("$.erros[0].mensagem").value("O titulo é obrigatorio!"));
+	}
+
+	@DisplayName("GET - Deve buscar o livro pelo isbn com sucesso.")
+	@Test
+	void deveBuscarLivroPeloIsbnComSucesso() throws Exception {
+
+		LocalDate dataFixa = LocalDate.of(2025, 8, 7);
+		String isbn = "9780132350884";
+
+		LivroDto dtoEsperado = new LivroDto(1L, "Clean Code", "Robert C. Martin", isbn, dataFixa, "Programação");
+
+		when(livroService.buscaLivroPeloIsbn(isbn)).thenReturn(dtoEsperado);
+
+		mockMvc.perform(get("/api/livros").param("isbn", isbn)).andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(1)).andExpect(jsonPath("$.titulo").value("Clean Code"))
+				.andExpect(jsonPath("$.autor").value("Robert C. Martin"))
+				.andExpect(jsonPath("$.isbn").value("9780132350884"))
+				.andExpect(jsonPath("$.categoria").value("Programação"));
+	}
+
+	@DisplayName("GET - Deve lançac exception se o isbn estiver ausente.")
+	@Test
+	void deveLancarExceptionSeIsbnEstiverAusente() throws Exception {
+		String isbn = "";
+
+		mockMvc.perform(get("/api/livros").param("isbn", isbn)).andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.mensagem").value("Campos inválidos")).andExpect(jsonPath("$.status").value(400))
+				.andExpect(jsonPath("$.path").value("/api/livros")).andExpect(jsonPath("$.timestamp").exists())
+				.andExpect(jsonPath("$.erros[0].campo").value(org.hamcrest.Matchers.endsWith("isbn")))
+				.andExpect(jsonPath("$.erros[0].mensagem").value("O isbn é obrigatorio!"));
+	}
+
+	@DisplayName("DELETE - Deve deletar um livro pelo isbn com sucesso.")
+	@Test
+	void deveDeletarUmLivroPeloIsbnComSucesso() throws Exception {
+		String isbn = "9780132350884";
+
+		doNothing().when(livroService).deletaLivroPeloIsbn(isbn);
+
+		mockMvc.perform(delete("/api/livros").param("isbn", isbn)).andExpect(status().isNoContent());
+	}
+
+	@DisplayName("DELETE - Deve lançar exception se o isbn estiver ausente.")
+	@Test
+	void deveLancarExceptionSeOIsbnEstiverAusente() throws Exception {
+		String isbn = "";
+
+		mockMvc.perform(delete("/api/livros").param("isbn", isbn)).andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.mensagem").value("Campos inválidos")).andExpect(jsonPath("$.status").value(400))
+				.andExpect(jsonPath("$.path").value("/api/livros")).andExpect(jsonPath("$.timestamp").exists())
+				.andExpect(jsonPath("$.erros[0].campo").value(org.hamcrest.Matchers.endsWith("isbn")))
+				.andExpect(jsonPath("$.erros[0].mensagem").value("O isbn é obrigatorio!"));
+		
+	}
+
+	@DisplayName("PUT - Deve atualizar um livro pelo isbn com sucesso")
+	@Test
+	void deveAtualizarLivroPeloIsbnComSucesso() throws Exception {
 		
 		LocalDate dataFixa = LocalDate.of(2025, 8, 7);
+		String isbn = "9780132350884";
+		
+		AtualizarLivroDto dtoEntrada = new AtualizarLivroDto("Clean Code", "Robert C. Martin", isbn, "Programação", dataFixa);
 
-		CriarLivroDto dtoEntrada = new CriarLivroDto("Clean Code", "Robert C. Martin", "9780132350884", "Programação");
+		LivroDto dtoEsperado = new LivroDto(1L, "Clean Code", "Robert C. Martin", isbn, dataFixa, "Programação");
 		
-		LivroDto dtoEsperado = new LivroDto(1L, "Clean Code", "Robert C. Martin", "9780132350884", dataFixa, "Programação");
-
+		when(livroService.atualizaLivroPeloIsBn(isbn, dtoEntrada)).thenReturn(dtoEsperado);
 		
-		when(livroService.criarLivro(dtoEntrada)).thenReturn(dtoEsperado);
-		
-		mockMvc.perform(post("/api/livros")
+		mockMvc.perform(put("/api/livros")
+				.param("isbn", isbn)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(dtoEntrada)))
-		.andExpect(status().isCreated())
+		.andExpect(status().isOk())
 		.andExpect(jsonPath("$.id").value(1))
 		.andExpect(jsonPath("$.titulo").value("Clean Code"))
 		.andExpect(jsonPath("$.autor").value("Robert C. Martin"))
@@ -64,13 +153,16 @@ class LivroControllerTest {
 		.andExpect(jsonPath("$.categoria").value("Programação"));
 	}
 	
-	@DisplayName("POST - Deve lançar exception se houver campo invalido.")
+	@DisplayName("PUT - Deve lançar exception caso o isbn esteja ausente.")
 	@Test
-	void deveLancarExceptionSeHouverCampoInvalido()throws Exception{
+	void deveLancarExceptionCasoIsbnEstejaAusente()throws Exception{
 		
-		CriarLivroDto dtoEntrada = new CriarLivroDto("", "Robert C. Martin", "9780132350884", "Programação");
-
-		mockMvc.perform(post("/api/livros")
+		LocalDate dataFixa = LocalDate.of(2025, 8, 7);
+		String isbn = "";
+		AtualizarLivroDto dtoEntrada = new AtualizarLivroDto("Clean Code", "Robert C. Martin", "9780132350884", "Programação", dataFixa);
+		
+		mockMvc.perform(put("/api/livros")
+				.param("isbn", isbn)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(dtoEntrada)))
 		.andExpect(status().isBadRequest())
@@ -78,48 +170,30 @@ class LivroControllerTest {
 		.andExpect(jsonPath("$.status").value(400))
 		.andExpect(jsonPath("$.path").value("/api/livros"))
 		.andExpect(jsonPath("$.timestamp").exists())
-		.andExpect(jsonPath("$.erros[0].campo").value("titulo"))
-		.andExpect(jsonPath("$.erros[0].mensagem").value("O titulo é obrigatorio!"));
-	}
+		.andExpect(jsonPath("$.erros[0].campo").value(org.hamcrest.Matchers.endsWith("isbn")))
+		.andExpect(jsonPath("$.erros[0].mensagem").value("O isbn é obrigatorio!"));
+		
+		
+	} 
 	
-	@DisplayName("GET - Deve buscar o livro pelo isbn com sucesso.")
+	@DisplayName("PUT - Deve lançar exception caso algum campo do Dto esteja ausente")
 	@Test
-	void deveBuscarLivroPeloIsbnComSucesso() throws Exception {
+	void deveLancarExceptionSeAlgumCampoDoDtoEstiverAusente() throws Exception{
 		
 		LocalDate dataFixa = LocalDate.of(2025, 8, 7);
 		String isbn = "9780132350884";
+		AtualizarLivroDto dtoEntrada = new AtualizarLivroDto("", "Robert C. Martin", isbn, "Programação", dataFixa);
 
-		LivroDto dtoEsperado = new LivroDto(
-			1L, "Clean Code", "Robert C. Martin", isbn,
-			dataFixa, "Programação"
-		);
-
-		when(livroService.buscaLivroPeloIsbn(isbn)).thenReturn(dtoEsperado);
-		
-		mockMvc.perform(get("/api/livros")
-				.param("isbn", isbn))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.id").value(1))
-			.andExpect(jsonPath("$.titulo").value("Clean Code"))
-			.andExpect(jsonPath("$.autor").value("Robert C. Martin"))
-			.andExpect(jsonPath("$.isbn").value("9780132350884"))
-			.andExpect(jsonPath("$.categoria").value("Programação"));
-	}
-	
-	@DisplayName("GET - Deve lançac exception se o isbn estiver ausente.")
-	@Test
-	void deveLancarExceptionSeIsbnEstiverAusente()throws Exception{
-		String isbn = "";
-		
-		mockMvc.perform(get("/api/livros")
-				.param("isbn", isbn))
+		mockMvc.perform(put("/api/livros")
+				.param("isbn", isbn)
+				.content(objectMapper.writeValueAsString(dtoEntrada))
+				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().isBadRequest())
 		.andExpect(jsonPath("$.mensagem").value("Campos inválidos"))
 		.andExpect(jsonPath("$.status").value(400))
 		.andExpect(jsonPath("$.path").value("/api/livros"))
 		.andExpect(jsonPath("$.timestamp").exists())
-		.andExpect(jsonPath("$.erros[0].campo").value("buscaLivroPeloIsbn.isbn"))
-		.andExpect(jsonPath("$.erros[0].mensagem").value("O isbn é obrigatorio!"));
+		.andExpect(jsonPath("$.erros[0].campo").value("titulo"))
+		.andExpect(jsonPath("$.erros[0].mensagem").value("O campo titulo é obrigatorio!"));
 	}
-	
 }
