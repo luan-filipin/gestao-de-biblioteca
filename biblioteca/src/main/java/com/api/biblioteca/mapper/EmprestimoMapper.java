d90773ebffed769fb5d653e83cf9e35b785177bd
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-08 11:04:01 -0300

    Mapper
    - Implementado o mapeamento dos campos.

--- biblioteca/src/main/java/com/api/biblioteca/mapper/EmprestimoMapper.java
+++ biblioteca/src/main/java/com/api/biblioteca/mapper/EmprestimoMapper.java
@@ -6,13 +6,20 @@ import org.mapstruct.Mapping;
 import com.api.biblioteca.dto.CriarEmprestimoDto;
 import com.api.biblioteca.dto.response.EmprestimoDto;
 import com.api.biblioteca.entity.Emprestimo;
+import com.api.biblioteca.entity.Livro;
+import com.api.biblioteca.entity.Usuario;
 
 @Mapper(componentModel = "spring")
 public interface EmprestimoMapper {
 
-	@Mapping(target = "usuario.id", source = "usuarioId")
-	@Mapping(target = "livro.id", source = "livroId")
-	Emprestimo toEntity(CriarEmprestimoDto dto);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dataEmprestimo", ignore = true)
+    @Mapping(target = "status", constant = "true")
+    @Mapping(target = "usuario", source = "usuario")
+    @Mapping(target = "livro", source = "livro")
+	Emprestimo toEntity(CriarEmprestimoDto dto, Usuario usuario, Livro livro);
 	
+    @Mapping(target = "nomeUsuario", source = "usuario.nome")
+    @Mapping(target = "tituloLivro", source = "livro.titulo")
 	EmprestimoDto toDto(Emprestimo entity);
 }
