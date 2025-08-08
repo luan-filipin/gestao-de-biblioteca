9b08488577501d040284cb115e57efa96b6938b9
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-08 11:05:47 -0300

    Dto
    - Organizado o código para melhor compreensão.
    - implementado a anotação @FutureOrPresent para que a data não possa ser retroativa.

--- biblioteca/src/main/java/com/api/biblioteca/dto/CriarEmprestimoDto.java
+++ biblioteca/src/main/java/com/api/biblioteca/dto/CriarEmprestimoDto.java
@@ -2,11 +2,18 @@ package com.api.biblioteca.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 
 public record CriarEmprestimoDto(
-	    @NotNull(message = "O ID do livro é obrigatório.") Long livroId,
-	    @NotNull(message = "O ID do usuário é obrigatório.") Long usuarioId,
-	    @NotNull(message = "A data de devolução é obrigatória.") LocalDate dataDevolucao
+	    @NotNull(message = "O ID do livro é obrigatório.") 
+	    Long livroId,
+	    
+	    @NotNull(message = "O ID do usuário é obrigatório.") 
+	    Long usuarioId,
+	    
+	    @FutureOrPresent(message = "A data não pode ser no passado.")
+	    @NotNull(message = "A data de devolução é obrigatória.")
+	    LocalDate dataDevolucao
 	    ) {
 }
