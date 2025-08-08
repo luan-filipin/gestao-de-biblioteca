f2e6a77c6ac2ae4d4a92a91353f7c25f6dd282ba
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-08 10:27:51 -0300

    Dto
    - Organizado o código para melhor compreensão.

--- biblioteca/src/main/java/com/api/biblioteca/dto/CriarLivroDto.java
+++ biblioteca/src/main/java/com/api/biblioteca/dto/CriarLivroDto.java
@@ -1,6 +1,9 @@
 package com.api.biblioteca.dto;
 
+import java.time.LocalDate;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CriarLivroDto(
 		@NotBlank(message = "O titulo é obrigatorio!")
@@ -10,5 +13,8 @@ public record CriarLivroDto(
 		@NotBlank(message = "O isbn é obrigatorio!")
 		String isbn,
 		@NotBlank(message = "O categoria é obrigatorio!")
-		String categoria) {
+		String categoria,
+		@NotNull(message = "A dataPublicacao é obrigatorio")
+		LocalDate dataPublicacao
+		) {
 }
