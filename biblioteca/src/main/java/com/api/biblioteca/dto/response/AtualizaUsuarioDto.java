6bba9089a9e5a6c6d7ff5c26c47c1e68d8b72ed6
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-08 14:47:39 -0300

    Dto
    - Removido email do Dto para que nao seja possivel alterar.

--- biblioteca/src/main/java/com/api/biblioteca/dto/response/AtualizaUsuarioDto.java
+++ biblioteca/src/main/java/com/api/biblioteca/dto/response/AtualizaUsuarioDto.java
@@ -1,16 +1,11 @@
 package com.api.biblioteca.dto.response;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record AtualizaUsuarioDto(
 		@NotBlank(message = "O nome é obrigatorio!") 
 		String nome, 
-		
-		@NotBlank(message = "O email é obrigatorio!")
-		@Email(message = "Deve ser um endereço valido!")
-		String email, 
 		
 		@NotBlank(message = "O telefone é obrigatorio!") 
 		@Pattern(regexp = "\\(?\\d{2}\\)?\\s?9?\\d{4}-?\\d{4}", message = "Telefone inválido. Formato esperado: (11)91234-5678")
