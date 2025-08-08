8c4654b4595bcc434498189165e9dffe27b2afb6
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-08 11:04:52 -0300

    Controller
    - Implementado a logica para o controller receber a requisição de criação de empréstimo.

--- biblioteca/src/main/java/com/api/biblioteca/controller/EmprestimoController.java
+++ biblioteca/src/main/java/com/api/biblioteca/controller/EmprestimoController.java
@@ -1,5 +1,29 @@
 package com.api.biblioteca.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.biblioteca.dto.CriarEmprestimoDto;
+import com.api.biblioteca.dto.response.EmprestimoDto;
+import com.api.biblioteca.service.EmprestimoService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/emprestimos")
 public class EmprestimoController {
 
+	private final EmprestimoService emprestimoService;
+	
+	@PostMapping
+	public ResponseEntity<EmprestimoDto> criaEmprestimo(@RequestBody @Valid CriarEmprestimoDto dto){
+		EmprestimoDto emprestimo = emprestimoService.criarEmprestimo(dto);		
+		return ResponseEntity.status(HttpStatus.CREATED).body(emprestimo);
+	}
 }
