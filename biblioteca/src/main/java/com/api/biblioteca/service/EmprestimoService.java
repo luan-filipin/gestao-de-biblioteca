405b295d0acf2f2729452217472c2cf542551ddd
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-08 14:50:33 -0300

    Service
    - Implementado metodos de atualizar empréstimos na interface.
    - Implementado logica no service para atualizar o empréstimo.

--- biblioteca/src/main/java/com/api/biblioteca/service/EmprestimoService.java
+++ biblioteca/src/main/java/com/api/biblioteca/service/EmprestimoService.java
@@ -1,10 +1,14 @@
 package com.api.biblioteca.service;
 
+import com.api.biblioteca.dto.AtualizarEmprestimoDto;
 import com.api.biblioteca.dto.CriarEmprestimoDto;
+import com.api.biblioteca.dto.response.AtualizarEmprestimoRespostaDto;
 import com.api.biblioteca.dto.response.EmprestimoDto;
 
 public interface EmprestimoService {
 
 	EmprestimoDto criarEmprestimo(CriarEmprestimoDto dto);
+	
+	AtualizarEmprestimoRespostaDto atualizarEmprestimoPeloId(Long id, AtualizarEmprestimoDto dto);
 	
 }
--- biblioteca/src/main/java/com/api/biblioteca/service/EmprestimoServiceImp.java
+++ biblioteca/src/main/java/com/api/biblioteca/service/EmprestimoServiceImp.java
@@ -2,13 +2,16 @@ package com.api.biblioteca.service;
 
 import org.springframework.stereotype.Service;
 
+import com.api.biblioteca.dto.AtualizarEmprestimoDto;
 import com.api.biblioteca.dto.CriarEmprestimoDto;
+import com.api.biblioteca.dto.response.AtualizarEmprestimoRespostaDto;
 import com.api.biblioteca.dto.response.EmprestimoDto;
 import com.api.biblioteca.entity.Emprestimo;
 import com.api.biblioteca.entity.Livro;
 import com.api.biblioteca.entity.Usuario;
 import com.api.biblioteca.mapper.EmprestimoMapper;
 import com.api.biblioteca.repository.EmprestimoRepository;
+import com.api.biblioteca.service.validador.EmprestimoValidador;
 import com.api.biblioteca.service.validador.LivroValidador;
 import com.api.biblioteca.service.validador.UsuarioValidador;
 
@@ -22,14 +25,24 @@ public class EmprestimoServiceImp implements EmprestimoService {
 	private final UsuarioValidador usuarioValidador;
 	private final LivroValidador livroValidador;
 	private final EmprestimoMapper emprestimoMapper;
+	private final EmprestimoValidador emprestimoValidador;
 
 	@Override
 	public EmprestimoDto criarEmprestimo(CriarEmprestimoDto dto) {
+		emprestimoValidador.validaSeLivroEstaEmprestado(dto.livroId());
 		Usuario usuario = usuarioValidador.validaSeIDDoUsuarioExiste(dto.usuarioId());
 		Livro livro = livroValidador.validaSeIdDoLivroExiste(dto.livroId());
 		Emprestimo emprestimo = emprestimoMapper.toEntity(dto, usuario, livro);
 		Emprestimo salvo = emprestimoRepository.save(emprestimo);
 		return emprestimoMapper.toDto(salvo);
+	}
+
+	@Override
+	public AtualizarEmprestimoRespostaDto atualizarEmprestimoPeloId(Long id, AtualizarEmprestimoDto dto) {
+		Emprestimo emprestimo = emprestimoValidador.buscaIdOuLancaException(id);
+		emprestimoMapper.atualizaDto(dto, emprestimo);
+		emprestimoRepository.save(emprestimo);
+		return emprestimoMapper.toAtualizarDto(emprestimo);
 	}
 
 }
