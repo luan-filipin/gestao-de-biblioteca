c65faf491328f319153e0ef0201b01aa51f8679a
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-08 20:02:37 -0300

    Repository
    - Implementado consulta JPQL

--- biblioteca/src/main/java/com/api/biblioteca/repository/LivroRepository.java
+++ biblioteca/src/main/java/com/api/biblioteca/repository/LivroRepository.java
@@ -1,16 +1,33 @@
 package com.api.biblioteca.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.api.biblioteca.entity.Livro;
 
-public interface LivroRepository extends JpaRepository<Livro, Long>{
+public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 	boolean existsByIsbn(String isbn);
-	
+
 	Optional<Livro> findByIsbn(String isbn);
-	
+
 	Optional<Livro> findById(Long id);
+
+	//consulta JPQL
+	@Query("""
+			    SELECT l FROM Livro l
+			    WHERE l.categoria IN (
+			        SELECT DISTINCT e.livro.categoria FROM Emprestimo e
+			        WHERE e.usuario.email = :email
+			    )
+			    AND l.id NOT IN (
+			        SELECT e.livro.id FROM Emprestimo e
+			        WHERE e.usuario.email = :email
+			    )
+			""")
+	List<Livro> recomendarLivrosPorCategoriaQueUsuarioAindaNaoLeu(@Param("email") String email);
 }
