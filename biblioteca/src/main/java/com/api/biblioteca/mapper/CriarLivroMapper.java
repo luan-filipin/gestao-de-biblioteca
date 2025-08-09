814524439b18455d3225940fe654b31c6d23a191
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-09 13:38:40 -0300

    Mapper
    - Implementado metodo para converter listas

--- biblioteca/src/main/java/com/api/biblioteca/mapper/CriarLivroMapper.java
+++ biblioteca/src/main/java/com/api/biblioteca/mapper/CriarLivroMapper.java
@@ -1,5 +1,7 @@
 package com.api.biblioteca.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
 import com.api.biblioteca.dto.CriarLivroDto;
@@ -11,4 +13,6 @@ public interface CriarLivroMapper {
 	CriarLivroDto toDto(Livro entity);
 	
 	Livro toEntity(CriarLivroDto dto);
+	
+	List<Livro> toListEntity(List<CriarLivroDto> list);
 }
