36fd9dc47dc1861276fbd2e41a6f70220aa87c67
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-17 17:05:50 -0300

    Update LivroServiceImp.java
    - Implementado a anotação @Transactional.

--- biblioteca/src/main/java/com/api/biblioteca/service/LivroServiceImp.java
+++ biblioteca/src/main/java/com/api/biblioteca/service/LivroServiceImp.java
@@ -11,6 +11,7 @@ import com.api.biblioteca.mapper.LivroMapper;
 import com.api.biblioteca.repository.LivroRepository;
 import com.api.biblioteca.service.validador.LivroValidador;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -22,6 +23,7 @@ public class LivroServiceImp implements LivroService{
 	private final CriarLivroMapper criarLivroMapper;
 	private final LivroMapper livroMapper;
 
+	@Transactional
 	@Override
 	public LivroDto criarLivro(CriarLivroDto dto) {
 		livroValidador.validaSeOLivroExiste(dto.isbn());		
@@ -35,12 +37,14 @@ public class LivroServiceImp implements LivroService{
 		return livroMapper.toDto(livro);
 	}
 
+	@Transactional
 	@Override
 	public void deletaLivroPeloIsbn(String isbn) {
 		Livro livro = livroValidador.buscaPorIsbnOuLancaException(isbn);
 		livroRepository.delete(livro);
 	}
 
+	@Transactional
 	@Override
 	public LivroDto atualizaLivroPeloIsBn(String isbn, AtualizarLivroDto dto) {
 		Livro livro = livroValidador.buscaPorIsbnOuLancaException(isbn);
